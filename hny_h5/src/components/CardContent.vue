556c895623070eca20a36ed0b5c9183322325a67
hadesxiong <fengshixiong890614@gmail.com>
2024-12-26 16:35:27 +0800

    update page structure

---
+++ hny_h5/src/components/CardContent.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="flex flex-col w-80 h-fit bg-cyan-500 items-center gap-4">
+        <div class="w-full h-12 bg-fuchsia-500"></div>
+        <div class="w-full h-fit bg-fuchsia-600 flex flex-col p-4 gap-4">
+            <div class="w-full h-12 bg-white"></div>
+            <div class="w-full h-fit bg-white flex gap-4">
+                <div class="w-6 h-40 bg-slate-500"></div>
+                <div class="w-6 h-40 bg-slate-500"></div>
+                <div class="w-6 h-40 bg-slate-500"></div>
+                <div class="w-6 h-40 bg-slate-500"></div>
+                <div class="w-6 h-40 bg-slate-500"></div>
+                <div class="w-auto flex-grow h-40 bg-orange-500"></div>
+            </div>
+            <div class="w-full h-1 bg-white"></div>
+            <div class="w-full h-auto flex-grow bg-white flex flex-col gap-4">
+                <div class="w-20 h-6 bg-slate-500"></div>
+                <div class="w-full h-16 bg-slate-500"></div>
+                <div class="w-20 h-6 bg-slate-500"></div>
+                <div class="w-full h-16 bg-slate-500"></div>
+            </div>
+        </div>
+        <div class="flex w-full h-fit justify-between gap-4">
+            <div class="w-full h-12 bg-white"></div>
+            <div class="w-full h-12 bg-red-500" @click.stop="test_close"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'CardContent',
+    methods: {
+        test_close() {
+            this.$emit(
+                'getCloseStatu',
+                {
+                    'close': true
+                }
+            )
+        }
+    }
+})
+</script>
--- hny_h5/src/view/ MainView.vue
+++ hny_h5/src/view/ MainView.vue
@@ -1,30 +1,53 @@
 <template>
-    <div class=" w-fit absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+    <!-- <div class="w-fit absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
         grid gap-4
-        md: grid-cols-4
-        lg: grid-cols-6
-        xl: grid-cols-8">
+        md:grid-cols-4
+        lg:grid-cols-6
+        xl:grid-cols-8">
         <main-card v-for="item in items" :key="item.id"></main-card>
+    </div> -->
+    <div class="fixed w-full h-full bg-amber-400 flex items-center justify-center flex-col">
+        <div class="w-40 h-60 bg-fuchsia-500"></div>
+        <div class="w-40 h-16 bg-blue-500 mt-12" @click.stop="test_click"></div>
+    </div>
+    <div v-if="qian_show" class="fixed z-20 w-full h-full bg-slate-700 flex items-center justify-center">
+        <card-con @getCloseStatu="handleCloseStatu"></card-con>
     </div>
 </template>
 
 <script>
 import { defineComponent, ref } from 'vue';
-import MainCard from '@/components/MainCard.vue';
+// import MainCard from '@/components/MainCard.vue';
+import CardContent from '@/components/CardContent.vue';
 
 export default defineComponent({
     name: 'MainView',
     components: {
-        'main-card': MainCard
+        // 'main-card': MainCard,
+        'card-con': CardContent
     },
     setup() {
-        const items = Array.from({ length: 48 }, (_, index) => ({
+        const items = Array.from({ length: 0 }, (_, index) => ({
             id: index, // 或者任何其他你想要为每个 div 设置的唯一标识符
             // 其他你需要的数据属性
         }));
+        const qian_loading = ref(false);
+        const qian_show = ref(false)
         const cardRefs = ref([]);
 
-        return {items, cardRefs};
+        return {items, cardRefs,
+            qian_loading, qian_show
+        };
+    },
+    methods: {
+        test_click() {
+            this.qian_show = !this.qian_show;
+            return this.qian_show;
+        },
+        handleCloseStatu(item) {
+            console.log(item)
+            this.qian_show = false;
+        }
     }
 })
 
